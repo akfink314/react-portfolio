4053b9d14687bd5ac113bc970a6ba8e6fc79f0c8
Alexandra <akfink@umich.edu>
2024-01-26 17:57:14 -0500

    chanked <a> to <Link>

--- components/AboutMe/index.js
+++ components/AboutMe/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import Link from 'next/link';
 
 const AboutMe = ({ img, name, description, onClick }) => {
     return (
       <div style={{ paddingLeft: '10px',paddingRight: '300px'}}>
         <p> My name is Alexandra. I am a senior at the University of Michigan where I study Computer Science and Computational Cognitive Science💻🧠 I previously interned at Amazon Robotics in Seattle, where I worked with an awesome team 
-          and launched an <a href='/projects#amazonproject' style={{color: 'blue', textDecoration: "underline"}} > internal tool </a> for Robot Visualization. I recieved an offer to return full time as a Software Engineer, but am looking for work in the East Coast. I love reading (checkout my Goodreads), camping, high adrenaline sports, and working with awesome people. 
+          and launched an <Link href='/projects#amazonproject'  >  internal tool  </Link> for Robot Visualization. I recieved an offer to return full time as a Software Engineer, but am looking for work in the East Coast. I love reading (checkout my Goodreads), camping, high adrenaline sports, and working with awesome people. 
         </p>
       </div>
     );
